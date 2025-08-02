cfac6fd8de2d4727de359d22ee8ece5c047174b3
Sampada <sampada.vyas@alixpartners.com>
2025-08-02 12:46:59 +0530

    sequence matters in the node

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 const express=require("express");
 const app=express();
-app.use("/",(req,res)=>{
-    res.send("Hello from the dashnpard")
-})
+// app.use("/",(req,res)=>{
+//     res.send("Hello from the dashnpard")
+// })
+
 app.use("/hello",(req,res)=>{
     res.send("Hello  Hello Hello");
+})
+app.use("/hello/2",(req,res)=>{
+    res.send("AbQARkAdABRAS")
 })
 app.use("/test",(req,res)=>{
 res.send("Hello from the server");
 })
+
+app.use("/",(req,res)=>{
+    res.send("Hello from the dashnpard")
+})
 app.listen(7777,()=>{
     console.log("Server is Suvvessfully listeming at 7777")
-})
+}) 
