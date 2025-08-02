747d6f8ee2a499d0a01f442439b19ab29823838c
Sampada <sampada.vyas@alixpartners.com>
2025-08-02 13:55:07 +0530

    multiple route handler

--- src/index.js
+++ src/index.js
@@ -1,24 +1,42 @@
 const express = require("express");
 const app = express();
 
-// http://localhost:7777/user?userId=101
-// http://localhost:7777/user?userId=101&passoword=33
-app.get("/user", (req, res) => {
-  console.log(req.query);
-  res.send({ firstName: "Akshay", lastName: "Saini" });
-});
-
-// http://localhost:7777/user/707
-app.get("/user/:userId", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Akshay", lastName: "Saini" });
-});
-
-
-app.get("/user/:userId/:name", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Akshay",  });
-});
+// app.get(
+//   "/user",
+//   (req, res, next) => {
+//     console.log("1");
+//     res.send({ firstName: "Akshay", lastName: "Saini" });
+//     next();
+//   },
+//   (req, res) => {
+//     console.log("2");
+//     res.send("2nd Response");
+//   }
+// );
+//new
+app.get("/user", [
+  (req, res, next) => {
+    console.log("1");
+    next();
+    // res.send({ firstName: "Akshay", lastName: "Saini" });
+  },
+  (req, res, next) => {
+    console.log("2");
+    next();
+    // res.send("2nd Response");
+  },
+  (req, res, next) => {
+    console.log("3");
+    next();
+    // res.send("2nd Response");
+  },
+  (req, res, next) => {
+    console.log("4");
+    // next();
+    // res.send("2nd Response");
+  },
+]);
+//Cannot GET /user
 
 app.listen(7777, () => {
   console.log("Server is Successfully listening on port 7777");
