c59a954ce2bd8f68173be6c3c2a030f90609f7bf
Sampada <sampada.vyas@alixpartners.com>
2025-08-03 22:58:22 +0530

    get APIS

--- src/index.js
+++ src/index.js
@@ -1,29 +1,50 @@
 const express = require("express");
 const connectDB = require("./config/database");
-const User=require("./models/user");
+const User = require("./models/user");
 const app = express();
-// const bodyParser = require('body-parser')
-// app.use(bodyParser.json()); 
-app.use(express.json()); 
 
-app.post("/signup", async(req, res) => {
- const userData= req.body;
+app.use(express.json());
+
+app.use("/user", async (req, res) => {
+  const emailId = req.body?.emailId;
+  try {
+    const user = await User.findOne({ emailId }); 
+    if (user.length == 0) {
+      res.send(404).send("User not found");
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(400).send("Something went Wrong");
+  }
+});
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.send("No data found");
+    } else {
+      res.send(users);
+    }
+  } catch (e) {
+     res.status(400).send("Something went Wrong");
+  }
+});
+
+app.post("/signup", async (req, res) => {
+  const userData = req.body;
   // const userObj = {
   //   firstName: "Akshay",
   //   lastName: "saini",
   //   emailId: "aksahy@saini.com",
   //   password: "akshat@123",
   // };
-  console.log(userData,"userData")
-  try{
-    
-    const user=new User(userData)
+  console.log(userData, "userData");
+  try {
+    const user = new User(userData);
     await user.save();
-    res.send("User Saved Successfully")
-  }
-  catch(err)
-  {
-    res.status(500).send("Error Saving the user")
+    res.send("User Saved Successfully");
+  } catch (err) {
+    res.status(500).send("Error Saving the user");
   }
 });
 
